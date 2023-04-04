509fb3941d857659f2bfab24e9aa5ec137eafe0c
Eug-Mart <62914548+Eug-Mart@users.noreply.github.com>
2023-04-04 00:24:18 -0300

    se agregan botones de menu

---
+++ Assets/Scripts/Player/Pause/PauseMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject buttomPause;
+    [SerializeField] private GameObject menuPause;
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        buttomPause.SetActive(false);
+        menuPause.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Continue()
+    {
+        buttomPause.SetActive(true);
+        menuPause.SetActive(false);
+    }
+
+}
